2b84a3a30843371c10dc6b770e1a4cd990d79388
junseok <juniboy97@naver.com>
2024-03-22 17:29:57 +0900

    refactor refactoring inference_controlnet.py

--- models/diffusers/examples/controlnet/inference_controlnet.py
+++ models/diffusers/examples/controlnet/inference_controlnet.py
@@ -5,7 +5,7 @@ import os
 import json
 
 base_model_path = "runwayml/stable-diffusion-v1-5"
-controlnet_path = "./model/controlnet_base"
+controlnet_path = "/home/junseok/model/controlnet_llava"
 
 controlnet = ControlNetModel.from_pretrained(controlnet_path, torch_dtype=torch.float16)
 pipe = StableDiffusionControlNetPipeline.from_pretrained(
@@ -19,10 +19,10 @@ pipe.enable_xformers_memory_efficient_attention()
 # memory optimization.
 pipe.enable_model_cpu_offload()
 
-image_list = os.listdir('/home/junseok/controlnet_dataset/test/images')
-conditioning_list = os.listdir('/home/junseok/controlnet_dataset/test/conditioning_images')
+image_list = os.listdir('/home/junseok/dataset/test/images')
+conditioning_list = os.listdir('/home/junseok/dataset/test/conditioning_images')
 # JSONL 파일 열기
-with open('/home/junseok/controlnet_dataset/test/metadata.jsonl', 'r') as f:
+with open('/home/junseok/dataset/test/metadata.jsonl', 'r') as f:
     # 각 라인을 읽어서 리스트에 저장
     lines = f.readlines()
 
@@ -33,9 +33,9 @@ for line in lines:
 
 os.makedirs('./model/output/', exist_ok=True)
 
-for i in range(10):
+for i in range(len(data)):
     file = data[i]['conditioning_image']
-    image_file = '/home/junseok/controlnet_dataset/test/conditioning_images/' + file
+    image_file = '/home/junseok/dataset/test/conditioning_images/' + file
     
     control_image = load_image(image_file)
     prompt = data[i]['text']
@@ -46,14 +46,3 @@ for i in range(10):
         prompt, num_inference_steps=20, generator=generator, image=control_image
     ).images[0]
     image.save("./model/output/output" + file)
-
-
-# control_image = load_image("./conditioning_image_1.png")
-# prompt = "pale golden rod circle with old lace background"
-
-# generate image
-# generator = torch.manual_seed(0)
-# image = pipe(
-#     prompt, num_inference_steps=20, generator=generator, image=control_image
-# ).images[0]
-# image.save("./model/output/output.png")
